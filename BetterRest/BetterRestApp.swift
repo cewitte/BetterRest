3f123239491c17cc84bd5acbfc4d0bc3930032e2
Carlos Eduardo Witte <cewitte@icloud.com>
2024-07-07 13:05:04 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Carlos Eduardo Witte on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
